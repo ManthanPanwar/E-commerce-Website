9bedc2c9e9691a5acb6bc576540f0d20249165d9
ManthanPanwar <manthandpr@gmail.com>
2024-08-07 00:27:03 +0530

    create Payment Model functionality

--- app.js
+++ app.js
@@ -1,9 +1,137 @@
 const sliderWrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: "$119",
+    colors: [
+      {
+        code: "black",
+        img: "./images/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./images/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: "$149",
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./images/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: "$109",
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./images/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: "$129",
+    colors: [
+      {
+        code: "black",
+        img: "./images/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./images/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: "$99",
+    colors: [
+      {
+        code: "gray",
+        img: "./images/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./images/hippie2.png",
+      },
+    ],
+  },
+];
+
+// it is initialized to products[0] bcoz when we open website we are on the first page and it should work for the first product.
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
-    console.log("click");
+    // change the current slide
     sliderWrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    // change the choosen product
+    choosenProduct = products[index];
+
+    // change text of current product
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0]?.img;
+
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct?.colors[index].code;
+    });
   });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    // we are able to select more than one size, lets resolve this
+    currentProductSizes.forEach((color, index) => {
+      color.style.backgroundColor = "white";
+      color.style.color = "black";
+    });
+    size.style.backgroundColor = "red";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+close.addEventListener("click", () => {
+  payment.style.display = "none";
 });
